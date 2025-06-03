aac4b90337c037ebc756947be1fe108e708c8900
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-06-03 23:56:18 +0600

    Explored string constructor

---
+++ stringConstructor.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // string ourString("Hello World", 5);
+    // string ourString = "Hello World";
+    // string anotherString(ourString, 4);
+
+    string ourString(5, 'A');
+
+    cout << ourString;
+
+    return 0;
+}

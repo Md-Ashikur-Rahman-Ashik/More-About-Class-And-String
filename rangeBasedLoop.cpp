fc5d46c1c349a9c2e055b31aae09019184a5d41e
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-06-04 00:15:14 +0600

    Explored range based for loop

---
+++ rangeBasedLoop.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string ourString;
+    cin >> ourString;
+
+    // int stringLength = ourString.size();
+
+    // for (int i = 0; i < stringLength; i++)
+    // {
+    //     /* code */
+    //     cout << ourString[i] << " ";
+    // }
+
+    for (char character: ourString)
+    {
+        /* code */
+        cout << character << " ";
+    }
+    
+
+    return 0;
+}

f92f3fed08f03f469222a1f1b8c08b81cad0089a
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-06-04 00:02:58 +0600

    Explored string sorting using sort() function

---
+++ sortString.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string ourString;
+    cin >> ourString;
+
+    sort(ourString.begin(), ourString.end());
+
+    cout << ourString << endl;
+
+    return 0;
+}
